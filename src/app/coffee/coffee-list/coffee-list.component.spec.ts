5c74cce041fb6f3c1337dee0ce11ae22b029cdf2
Mauricio Rodriguez <am.rodriguezs1@uniandes.edu.co>
2025-04-27 13:27:01 -0500

    feat: add test

--- src/app/coffee/coffee-list/coffee-list.component.spec.ts
+++ src/app/coffee/coffee-list/coffee-list.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CoffeeListComponent } from './coffee-list.component';
+import { CoffeeService } from '../services/coffee.service';
+import { of } from 'rxjs';
+import { Coffee } from '../models/coffee';
 
 describe('CoffeeListComponent', () => {
   let component: CoffeeListComponent;
   let fixture: ComponentFixture<CoffeeListComponent>;
+  let mockCoffeeService: jasmine.SpyObj<CoffeeService>;
 
   beforeEach(async () => {
+    // Creamos un mock del servicio
+    const coffeeServiceSpy = jasmine.createSpyObj('CoffeeService', ['getCoffees']);
+
     await TestBed.configureTestingModule({
-      imports: [CoffeeListComponent]
-    })
-    .compileComponents();
+      declarations: [CoffeeListComponent],
+      providers: [
+        { provide: CoffeeService, useValue: coffeeServiceSpy }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CoffeeListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockCoffeeService = TestBed.inject(CoffeeService) as jasmine.SpyObj<CoffeeService>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load coffees on init', () => {
+    const mockCoffees: Coffee[] = [
+      new Coffee(1, 'Coffee 1', 'Tipo 1', 'Región 1', 'Sabor 1', 1000, 'imagen1.jpg'),
+      new Coffee(2, 'Coffee 2', 'Tipo 2', 'Región 2', 'Sabor 2', 1200, 'imagen2.jpg')
+    ];
+
+    // Hacemos que el servicio retorne nuestro mock
+    mockCoffeeService.getCoffees.and.returnValue(of(mockCoffees));
+
+    // Disparamos el ngOnInit
+    fixture.detectChanges();
+
+    // Verificamos que se asignaron los cafés
+    expect(component.coffees.length).toBe(2);
+    expect(component.coffees).toEqual(mockCoffees);
+    expect(mockCoffeeService.getCoffees).toHaveBeenCalled();
   });
 });
--- src/app/coffee/services/coffee.service.spec.ts
+++ src/app/coffee/services/coffee.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CoffeeService } from './coffee.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Coffee } from '../models/coffee';
 
 describe('CoffeeService', () => {
   let service: CoffeeService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CoffeeService]
+    });
+
     service = TestBed.inject(CoffeeService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // Verifica que no haya peticiones pendientes
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch and map coffees correctly', () => {
+    const mockResponse = [
+      {
+        id: 1,
+        nombre: 'Coffee 1',
+        tipo: 'Tipo 1',
+        region: 'Región 1',
+        sabor: 'Sabor 1',
+        altura: 1000,
+        imagen: 'imagen1.jpg'
+      },
+      {
+        id: 2,
+        nombre: 'Coffee 2',
+        tipo: 'Tipo 2',
+        region: 'Región 2',
+        sabor: 'Sabor 2',
+        altura: 1200,
+        imagen: 'imagen2.jpg'
+      }
+    ];
+
+    service.getCoffees().subscribe(coffees => {
+      expect(coffees.length).toBe(2);
+      expect(coffees[0]).toEqual(jasmine.any(Coffee));
+      expect(coffees[0].name).toBe('Coffee 1');
+      expect(coffees[1].region).toBe('Región 2');
+    });
+
+    const req = httpMock.expectOne(service['apiUrl']);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse); // Respondemos a la petición con los datos mockeados
   });
 });
